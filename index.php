e7509825506ccbdda1c4ccbef44b2c8d0e3446ea
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-10 15:46:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php 
+// Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+// L'e-commerce vende prodotti per animali.
+// I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+// I prodotti saranno oltre al cibo, anche giochi, cucce, etc.
+// Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria
+// ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).
+// BONUS (Opzionale):
+// Il cliente potrà sia comprare i prodotti come ospite, senza doversi registrarsi nello store, oppure può iscriversi
+// e creare un account per ricevere cosi il 20% di sconto.
+// Il cliente effettua il pagamento dei prodotti nel carrello con la carta di credito, che non deve essere scaduta.
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="ginatovaralmario">
+    <meta name="project" content="php8-reditarietà">
+    <title>Document</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center mt-3">
+                <h1>PHP OOP 2 </h1>
+            </div>
+            <div class="col">
+                <div class="card mt-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+</body>
+</html>
